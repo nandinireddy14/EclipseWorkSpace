869ff98a691256dcbe522c2e40ab2f4f1cc9e9d2
nandinireddy14 <123911788+nandinireddy14@users.noreply.github.com>
2023-06-17 10:48:03 +0530

    Update BranchTest.java

--- NewJavaFeaturesProject/src/cgg/branching/BranchTest.java
+++ NewJavaFeaturesProject/src/cgg/branching/BranchTest.java
@@ -6,7 +6,9 @@ public class BranchTest {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");//master c4 commit
 		System.out.println("Hey..Hiii");//brach1 c5 commit
-		System.out.println("Did changes on github");//branch11
+		System.out.println("Did changes on github (Fetch & merge)");//branch11
+		System.out.println("For Pulling");//branch12
+		
 
 	}
 
