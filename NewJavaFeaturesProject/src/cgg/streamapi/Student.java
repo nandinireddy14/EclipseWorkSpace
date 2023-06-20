063ebddaa0f5d598bd9cc5d65e9d311dd23d9b11
nandini <nandinireddyjillala14@gmail.com>
2023-06-20 15:58:24 +0530

    worked on streams

--- NewJavaFeaturesProject/src/cgg/streamapi/Student.java
+++ NewJavaFeaturesProject/src/cgg/streamapi/Student.java
@@ -22,15 +22,11 @@ public class Student {
 		public String getName() {
 			return name;
 		}
-		public void setName(String name) {
-			this.name = name;
-		}
+
 		public int getAge() {
 			return age;
 		}
-		public void setAge(int age) {
-			this.age = age;
-		}
+
 		@Override
 		public String toString() {
 			return "Student [name=" + name + ", age=" + age + "]";
